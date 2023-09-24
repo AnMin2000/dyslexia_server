5d8605011c292ad261b7781f2c54faeb02fd1b9d
AnMin <iededg@naver.com>
2023-09-24 15:44:06 +0900

    ver.3.5.5 앨범 만드는중 (웹페이지 테스트 하기전 코드 저장)

--- src/main/java/com/codingrecipe/project01/controller/ImageController.java
+++ src/main/java/com/codingrecipe/project01/controller/ImageController.java
@@ -2,32 +2,21 @@ package com.codingrecipe.project01.controller;
 
 import com.codingrecipe.project01.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
+import reactor.core.publisher.Flux;
+import java.util.List;
 
 @RestController
 public class ImageController {
-    @GetMapping("/getImage")
-    public ResponseEntity<byte[]> getImage() throws IOException {
-        String imagePath = "C:\\Users\\user\\Desktop\\Album\\JPEG_20230913_213712_5601104748635264103.jpg";
-        File imageFile = new File(imagePath);
 
-        byte[] imageBytes = Files.readAllBytes(imageFile.toPath());
+    private ImageService imageService;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-
-        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+    @GetMapping(value = "/getImages/{userID}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public Flux<byte[]> getImages(@PathVariable String userID) {
+        List<byte[]> images = imageService.getImages(userID);
+        return Flux.fromIterable(images);
     }
 }
-
---
+++ src/main/java/com/codingrecipe/project01/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.codingrecipe.project01.repository;
+
+import java.util.List;
+
+public class ImageRepository {
+    public List<String> findImagePathsByUserID(String userID) {
+        return null;
+    }
+}
