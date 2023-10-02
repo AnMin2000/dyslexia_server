5fe3316c865e02861fb1e1b6e7a37dcc23a3164a
AnMin <iededg@naver.com>
2023-10-02 21:57:53 +0900

    ver.3.6.1 서버에서 안드로이드로 사진 한장 보냄 -> 두장 보내기 성공(동적으로 ImageView 늘어나는 건 아직 안 됨)

--- src/main/java/com/codingrecipe/project01/controller/PhotoController.java
+++ src/main/java/com/codingrecipe/project01/controller/PhotoController.java
@@ -1,34 +1,40 @@
 package com.codingrecipe.project01.controller;
 
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 @RestController
 public class PhotoController {
 
-    @GetMapping("/getPhoto")
-    public ResponseEntity<Resource> getPhoto() throws MalformedURLException {
-        // 파일 경로 설정
-        Path filePath = Paths.get("C:/Users/user/Desktop/Album/JPEG_20230911.jpg");
-        Resource resource = new UrlResource(filePath.toUri());
+    @GetMapping("/getPhotos")
+    public ResponseEntity<List<String>> getPhotos() throws IOException {
+        List<String> photoList = new ArrayList<>();
 
-        // 파일을 불러와서 HTTP 응답으로 전송
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=JPEG_20230911.jpg");
+        // 이미지 파일들의 경로를 배열에 추가
+        String[] imagePaths = {
+                "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+                "C:/Users/user/Desktop/Album/JPEG_20230911.jpg"
+                // 다른 이미지 경로도 추가 가능
+        };
+
+        for (String imagePath : imagePaths) {
+            byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
+            String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
+            photoList.add(encodedImage);
+        }
 
         return ResponseEntity.ok()
-                .headers(headers)
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(resource);
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(photoList);
     }
 }
