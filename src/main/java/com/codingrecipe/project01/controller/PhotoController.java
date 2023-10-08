0e85afd1a598e4f644dd17cdfdf81a5eb939d02f
AnMin <iededg@naver.com>
2023-10-08 23:41:44 +0900

    ver.3.6.4 사진 한번에 -> 하나하나 낱개로 수정 err : 사진이 일렬로 내려가고 사진 로딩이 너무 느림 Lazy 고려

--- src/main/java/com/codingrecipe/project01/controller/PhotoController.java
+++ src/main/java/com/codingrecipe/project01/controller/PhotoController.java
@@ -3,38 +3,61 @@ package com.codingrecipe.project01.controller;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
 
 @RestController
 public class PhotoController {
 
-    @GetMapping("/getPhotos")
-    public ResponseEntity<List<String>> getPhotos() throws IOException {
-        List<String> photoList = new ArrayList<>();
+    private final String[] imagePaths = {
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
+            "C:/Users/user/Desktop/Album/JPEG_20230911.jpg"
+            // 다른 이미지 경로도 추가 가능
+    };
 
-        // 이미지 파일들의 경로를 배열에 추가
-        String[] imagePaths = {
-                "C:/Users/user/Desktop/Album/JPEG_20230911.jpg",
-                "C:/Users/user/Desktop/Album/JPEG_20230911.jpg"
-                // 다른 이미지 경로도 추가 가능
-        };
-
-        for (String imagePath : imagePaths) {
-            byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
-            String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
-            photoList.add(encodedImage);
-        }
-
+    @GetMapping("/getPhotoCount")
+    public ResponseEntity<Integer> getPhotoCount() {
+        int photoCount = imagePaths.length;
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(photoList);
+                .body(photoCount);
+    }
+
+    @GetMapping("/getPhoto")
+    public ResponseEntity<String> getPhoto(@RequestParam("index") int index) throws IOException {
+        if (index >= 0 && index < imagePaths.length) {
+            String imagePath = imagePaths[index];
+            byte[] imageBytes = Files.readAllBytes(Paths.get(imagePath));
+            String encodedImage = Base64.getEncoder().encodeToString(imageBytes);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(encodedImage);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
